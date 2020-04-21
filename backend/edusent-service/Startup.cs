4b45ae270091d7a3377f1e4c30250d10fff953c6
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2020-04-21 04:42:58 -0400

    Startup.cs

--- backend/edusent-service/Startup.cs
+++ backend/edusent-service/Startup.cs
@@ -139,7 +139,7 @@ namespace edusent_service
             });
 
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
