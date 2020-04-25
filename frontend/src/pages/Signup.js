ea8be352e6044295b8f30e79bdee44c5b33475b3
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2020-04-25 12:17:04 -0400

    updated git ignore

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -8,14 +8,12 @@ import { apiFetch } from "../utils/fetchLight";
 import SiteMargin from "../ui/SiteMargin";
 import Stack from "../ui/Stack";
 
-const signupSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email address").required("Email Required"),
-  password: Yup.string()
-    .min(8, "Password must be at least 8 characters long.")
-    .required("Password required."),
-});
+const signupSchema = Yup.object ().shape ( {
+    email: Yup.string ().email ( "Invalid email address" ).required ( "Email Required" ),
+    password: Yup.string ().min ( 8, "Password must be at least 8 characters long." ).required ( "Password required." )
+} );
 
-const StyledForm = styled.div`
+const StyledForm = styled.div `
   & > form {
     width: 750px;
     margin: auto;
@@ -25,73 +23,64 @@ const StyledForm = styled.div`
   }
 `;
 
-const Signup = ({ navigate }: Object) => {
-  return (
-    <SiteMargin>
-      <Formik
-        validateOnChange
-        validationSchema={signupSchema}
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        onSubmit={(formValues, formikBag) => {
-          apiFetch("users/signup", "POST", formValues)
-            .then(() => {
-              navigate(`/`);
-            })
-            .catch(async (error) => {
-              const body = await error.response.json();
-              if (body.message) {
-                formikBag.setStatus(body.message);
-              }
-            })
-            .finally(() => formikBag.setSubmitting(false));
-        }}
-      >
-        {({ isSubmitting, isValid, status }) => (
-          <StyledForm>
-            <Form>
-              <Typography variant="h1">Signup</Typography>
-              <Stack>
-                <br />
-                {status && <h4 style={{ color: "red" }}>{status}</h4>}
-                <Field
-                  id="email"
-                  name="email"
-                  label="Email"
-                  component={Input}
-                  variant="outlined"
-                  placeholder="Email"
-                  fullWidth
-                />
-                <Field
-                  id="password"
-                  name="password"
-                  label="Password"
-                  component={Input}
-                  variant="outlined"
-                  placeholder="Password"
-                  type="Password"
-                  fullWidth
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  color="primary"
-                  variant="contained"
-                  align="right"
-                  disabled={isSubmitting || !isValid}
-                >
-                  Submit
-                </Button>
-              </Stack>
-            </Form>
-          </StyledForm>
-        )}
-      </Formik>
-    </SiteMargin>
-  );
+const Signup = ( { navigate } : Object ) => {
+    return (
+        <SiteMargin>
+            <Formik validateOnChange
+                validationSchema={signupSchema}
+                initialValues={
+                    {
+                        email: "",
+                        password: ""
+                    }
+                }
+                onSubmit={
+                    ( formValues, formikBag ) => {
+                        apiFetch ( "users/signup", "POST", formValues ).then ( () => {
+                            navigate ( `/` );
+                        } ).catch ( async ( error ) => {
+                            const body = await error.response.json ();
+                            if ( body.message ) {
+                                formikBag.setStatus ( body.message );
+                            }
+                        } ). finally ( () => formikBag.setSubmitting ( false ) );
+                    }
+            }>
+                {
+                ( { isSubmitting, isValid, status } ) => (
+                    <StyledForm>
+                        <Form>
+                            <Typography variant="h1">Signup</Typography>
+                            <Stack>
+                                <br/> {
+                                status && <h4 style={
+                                    { color: "red" };
+                                }>
+                                    {status}</h4>;
+                            }
+                                <Field id="email" name="email" label="Email"
+                                    component={Input}
+                                    variant="outlined"
+                                    placeholder="Email"
+                                    fullWidth/>
+                                <Field id="password" name="password" label="Password"
+                                    component={Input}
+                                    variant="outlined"
+                                    placeholder="Password"
+                                    type="Password"
+                                    fullWidth/>
+                                <Button type="submit" fullWidth color="primary" variant="contained" align="right"
+                                    disabled={
+                                        isSubmitting || !isValid;
+                                }>
+                                    Submit
+                                </Button>
+                            </Stack>
+                        </Form>
+                    </StyledForm>
+                );
+            } </Formik>
+        </SiteMargin>
+    );
 };
-
 export default Signup;
